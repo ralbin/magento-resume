0b9b4f1bf3fd8618b00e54e69a80756e0cd5e806
ralbin <russell@russellalbin.com>
2019-02-15 23:15:27 -0600

    Making the display more modular to handle the xml or database versions

--- Block/AbstractResume.php
+++ Block/AbstractResume.php
@@ -2,12 +2,27 @@
 namespace RussellAlbin\Resume\Block;
 
 use Magento\Framework\View\Element\Template;
+use RussellAlbin\Resume\Api\Data\ResumeInterface;
 
 abstract class AbstractResume extends Template
 {
 
     /**
-     * @return \SimpleXMLElement
+     * @var ResumeInterface
+     */
+    protected $resume;
+
+    public function __construct(
+        \Magento\Framework\View\Element\Template\Context $context,
+        ResumeInterface $resume,
+        array $data = []
+    ) {
+        $this->resume = $resume;
+        parent::__construct($context, $data);
+    }
+
+    /**
+     * @return ResumeInterface
      */
     protected function getResumeXml()
     {
@@ -16,6 +31,13 @@ abstract class AbstractResume extends Template
 
         // create a simplexml object from the contents of the current file
         $xml = simplexml_load_string($xml_file);
-        return $xml;
+
+        $this->resume->setFirstname($xml->personal_information->firstname);
+        $this->resume->setLastname($xml->personal_information->lastname);
+        $this->resume->setEmail($xml->personal_information->email);
+        $this->resume->setPhone($xml->personal_information->cell_phone);
+        $this->resume->setSkills($xml->personal_information->skills);
+        $this->resume->setObjective($xml->personal_information->objective);
+        return $this->resume;
     }
 }
--- Block/Resume.php
+++ Block/Resume.php
@@ -1,26 +1,14 @@
 <?php
 namespace RussellAlbin\Resume\Block;
 
-use RussellAlbin\Resume\Api\Data\ResumeInterface;
-
 class Resume extends AbstractResume
 {
+
     /**
-     * @var ResumeInterface
+     * @return \RussellAlbin\Resume\Api\Data\ResumeInterface
      */
-    protected $resume;
-
-    public function __construct(
-        \Magento\Framework\View\Element\Template\Context $context,
-        ResumeInterface $resume,
-        array $data = []
-    ) {
-        $this->resume = $resume;
-        parent::__construct($context, $data);
-    }
-
     public function getResumeData()
     {
-        $this->getResumeXml();
+        return $this->getResumeXml();
     }
 }
